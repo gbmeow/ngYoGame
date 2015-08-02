2c428a64d6d3b9399d3dee97065a67ac6a249757
Jerzy Batalinski <george.batalinski@gmail.com>
2015-08-02 16:21:14 -0400

    webpack setup testing

--- client/directives/game.directive.js
+++ client/directives/game.directive.js
@@ -18,7 +18,39 @@ module.exports = function(ngModule) {
         }
         function gameCtrl() {
             var vm = this;
-            vm.greeting = 'Start Game';
+
+            var game =
+                new Phaser.Game(
+                    800, 600,         // width x height
+                    Phaser.AUTO,      // the game context, 2D/3D
+                    'game_canvas',     // id of the DOM element to add the game
+                    { preload: preload, create: create, update: function() {} }
+                );
+
+            function preload () {
+                game.load.image('yeoman', '../images/yeoman.png');
+            }
+
+            function create()  {
+                addSprite();
+            }
+
+
+
+            vm.move = function (key) {
+                console.log(key);
+            }
+
+            function getRandomInt() {
+                var min = 1;
+                var max = 100;
+                return Math.floor(Math.random() * (max - min)) + min;
+            }
+
+            function addSprite() {
+                return game.add.sprite(10, getRandomInt(), 'dude');
+            }
         }
+
     }
 }
